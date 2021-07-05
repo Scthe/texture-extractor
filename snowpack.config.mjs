c046ba4e0a23e0aad29509304872d95aad7fd9a9
Scthe <marcin1113C@gmail.com>
2021-07-05 20:15:43 +0200

    Jekyll, wtf?

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -27,7 +27,7 @@ export default {
   ],
   optimize: {
     /* Example: Bundle your final build: */
-    bundle: true,
+    bundle: false,
     minify: true,
     target: "es2018",
     splitting: true,
